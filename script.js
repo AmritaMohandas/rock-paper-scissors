b5d95bc72f84e250000e02b3f50781ea75574fb8
AmritaMohandas <amrita.a.mohandas@gmail.com>
2025-08-25 18:25:23 -0500

    updated logic to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+function getComputerChoice() {
+
+    let randomVal = Math.floor(Math.random() * 3);
+    let choice = "";
+
+    if (randomVal == 0) {
+        choice = "rock";
+    }
+    else if (randomVal == 1) {
+        choice = "paper";
+    }
+    else if (randomVal == 2) {
+        choice = "scissors";
+    }
+
+    return choice;
+};
+
+console.log(getComputerChoice());
