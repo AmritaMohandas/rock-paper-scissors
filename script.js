1d29236dc8b62ec96812ee7cf92b733c00c4fcad
AmritaMohandas <amrita.a.mohandas@gmail.com>
2025-08-25 18:28:27 -0500

    declared players score variables and added logic to play multiple rounds of game

--- script.js
+++ script.js
@@ -21,5 +21,35 @@ function getHumanChoice() {
     return choice;
 };
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice == computerChoice) {
+        console.log(`You tie! ${humanChoice} ties with ${computerChoice}.`);
+    }
+    else if (humanChoice == "rock" && computerChoice == "scissors" ||
+            humanChoice == "paper" && computerChoice == "rock" ||
+            humanChoice == "scissors" && computerChoice == "paper") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    }
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    }
+
+    return humanScore, computerScore;
+};
+
+function playGame() {
+    for (let i = 1; i <= 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        humanScore, computerScore = playRound(humanSelection, computerSelection);
+    }
+    console.log(humanScore, computerScore);
+};
+
+let humanScore = 0;
+let computerScore = 0;
+playGame();
